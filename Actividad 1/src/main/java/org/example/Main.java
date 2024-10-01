b400b90edddbe84e3745d91142792cfaa68ced7b
A617\1DAW <alegonga2004@gmail.com>
2024-10-01 17:19:16 +0200

    no message

---
+++ Actividad 1/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        /*Operadores operadores = new Operadores();
+        operadores.Aritmeticos();*/
+        
+        Practica3 practicas3 = new Practica3();
+        practicas3.Practica3();
+    }
+}
