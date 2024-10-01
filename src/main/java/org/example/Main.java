012ffac5571178e65c37c6640a2e0d6803115ee8
A617\1DAW <alegonga2004@gmail.com>
2024-10-01 16:53:33 +0200

    no message

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.printf("¿Como te llamas?: ");
+        String nombre = entrada.next();
+        System.out.printf("Hola "+nombre+". Introduce tu edad: ");
+        int edad = entrada.nextInt();
+        while(edad < 0)
+        {
+            System.out.printf("Introduce de nuevo tu edad: ");
+            edad = entrada.nextInt();
+        }
+        if (edad >= 18){
+            System.out.println("Enhorabuena "+nombre+" puedes votar.");
+        }else{
+            System.out.println("Lo siento "+nombre+". No puedes votar.");
+            System.out.println("Te faltan "+(18-edad)+" años para ser mayor de edad");
+        }
+    }
+}
