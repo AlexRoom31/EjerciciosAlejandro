1d8757a2eb0179968bc9cd0c2b769222cbb2de53
A617\1DAW <alegonga2004@gmail.com>
2024-10-17 21:12:13 +0200

    Calculadora 1.2

--- Actividad 1/src/main/java/org/example/Calculadora.java
+++ Actividad 1/src/main/java/org/example/Calculadora.java
@@ -5,30 +5,50 @@ import java.util.Scanner;
 public class Calculadora {
     public void Calculadora(){
         Scanner sc = new Scanner(System.in);
-        Boolean err = true;
+        Boolean error = true;
         int num1 = 0;
         int num2 = 0;
+        int resultado = 0;
+        String operador = "+";
         System.out.println("BIENVENIDO A LA CALCULADORA RAPIDA");
-        while(err){
+        while(error){
             try {
                 System.out.println("Introduce el primer operando");
                 num1 = sc.nextInt();
                 System.out.println("Introduce signo a operar (+,-,/,x,R)");
-                String operador = sc.next();
+                operador = sc.next();
                 System.out.println("Introduce el segundo operando");
                 num2 = sc.nextInt();
                 if (operador.length()==1){
-                    if (operador == "+" || operador == "-" || operador == "/" || operador == "x" || operador == "R"){
-                        err = false;
+                    if (operador.equals("+") || operador.equals("-") || operador.equals("/") || operador.equals("x") || operador.equals("R")){
+                        error = false;
                     }
                 }
-                if (err){
+                if (error){
                     System.out.println("Error valores invalidos");
                 }
             }catch (Exception e){
                 System.out.println("Error valores invalidos");
             }
         }
+        switch (operador){
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "/":
+                resultado = num1 / num2;
+                break;
+            case "x":
+                resultado = num1 * num2;
+                break;
+            case "R":
+
+                break;
+        }
+        System.out.println("El resultado es: " + resultado);
 
     }
 }
