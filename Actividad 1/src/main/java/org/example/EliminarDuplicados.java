722a9bcf5c5b92023d79b41f12302cdcc241b13b
A617\1DAW <alegonga2004@gmail.com>
2024-11-15 18:12:31 +0100

    Eliminar Duplicados (terminado)

--- Actividad 1/src/main/java/org/example/EliminarDuplicados.java
+++ Actividad 1/src/main/java/org/example/EliminarDuplicados.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class EliminarDuplicados {
     public void Eliminar(){
         int[] vector = {1,1,6,7,8,8,10,15,15};
         int[] vectorAux = new int[vector.length];
         int num;
-
+        int tamaño = 0;
+        System.out.println(Arrays.toString(vector));
         for (int i = 0;i < vector.length;i++)
         {
             if (i != vector.length-1 && vector[i] == vector[i+1])
@@ -13,9 +16,21 @@ public class EliminarDuplicados {
                 System.out.println("Duplicado " + vector[i]);
             }else{
                 vectorAux[i] = vector[i];
+                tamaño++;
             }
 
-
         }
+        System.out.println(Arrays.toString(vectorAux));
+
+        int[] vectorLimpio = new int[tamaño];
+        int pos = 0;
+        for (int j = 0; j < vectorAux.length;j++)
+        {
+            if (vectorAux[j]!=0){
+                vectorLimpio[pos]=vectorAux[j];
+                pos++;
+            }
+        }
+        System.out.println(Arrays.toString(vectorLimpio));
     }
 }
