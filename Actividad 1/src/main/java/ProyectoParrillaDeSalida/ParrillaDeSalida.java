e2377293d8c53778ff2469a790bff5c27852425d
A617\1DAW <alegonga2004@gmail.com>
2024-12-05 21:16:22 +0100

    ParrilaDeSalida 0.1

--- Actividad 1/src/main/java/ProyectoParrillaDeSalida/ParrillaDeSalida.java
+++ Actividad 1/src/main/java/ProyectoParrillaDeSalida/ParrillaDeSalida.java
@@ -1,7 +1,33 @@
 package ProyectoParrillaDeSalida;
 
+import java.util.Scanner;
+
 public class ParrillaDeSalida {
     public static void parrilaDeSalida(){
-        
+        int numeroPilotos;
+
+        System.out.println("PARRILLA DE SALIDA");
+
+        System.out.println("Escriba el numero de pilotos que han terminado la carrera: ");
+        numeroPilotos = pedirNumero();
+    }
+
+    private static int pedirNumero(){
+        Scanner sc = new Scanner(System.in);
+        int numero = 0;
+        String numeroStr;
+        boolean valorCorrecto;
+
+        do {
+            numeroStr = sc.nextLine();
+            valorCorrecto=numeroStr.matches("[0-26]");
+            if (!valorCorrecto)
+            {
+                System.out.println("Error. Tiene que ingresar un numero del 0 al 26: ");
+            }
+        }while(!valorCorrecto);
+
+
+        return numero;
     }
 }
