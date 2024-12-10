1a21a4426b0dec814a84a6db14cb495114d81286
ALEJANDRO\alego <alegonga2004@gmail.com>
2024-12-10 20:43:45 +0100

    ParrilaDeSalida 1.1 (Pre- reconstruirParrilla)

--- Actividad 1/src/main/java/ProyectoParrillaDeSalida/ParrillaDeSalida.java
+++ Actividad 1/src/main/java/ProyectoParrillaDeSalida/ParrillaDeSalida.java
@@ -14,7 +14,7 @@ public class ParrillaDeSalida {
         System.out.println("Escriba el numero de pilotos que han terminado la carrera: ");
         numeroPilotos = pedirNumeroPilotos(MAXIMO_PILOTOS);
         System.out.println("Vamos a ingresar los resultados de la carrera con el nombre del piloto en orden de llegada y el numero de posiciones que a subido respecto a la parrilla de salida.");
-        String[][] pilotos = pedirPilotos(numeroPilotos);
+        String[][] pilotos = pedirPilotos(numeroPilotos,MAXIMO_PILOTOS);
 
 
     }
@@ -49,20 +49,29 @@ public class ParrillaDeSalida {
         return numero >= minimo && numero <= maximo;
     }
 
-    public static String[][] pedirPilotos(int numeroPilotos) {
+    public static String[][] pedirPilotos(int numeroPilotos,int maximoPilotos) {
         Scanner sc = new Scanner(System.in);
         String[][] pilotos = new String[2][numeroPilotos]; // Matriz 2xN: [0] para nombres, [1] para posiciones
 
         for (int i = 0; i < numeroPilotos; i++) {
-            System.out.print("Ingrese el nombre del "+(i + 1)+"º piloto: ");
+            System.out.print("Ingrese el nombre del piloto " + (i + 1) + ": ");
             pilotos[0][i] = sc.nextLine(); // Guardar nombre del piloto
 
-            System.out.print("Ingrese el cambio de posiciones: " + pilotos[0][i] + ": ");
+            int posicionesMaxAdelantar = maximoPilotos - (i + 1); // Máximo que puede adelantar
+            int posicionesMaxPerder = i; // Máximo que puede perder
+
+            System.out.print("Ingrese cuántas posiciones ha adelantado o perdido " + pilotos[0][i] + ": ");
             while (true) {
                 String posiciones = sc.nextLine();
-                if (posiciones.matches("-?\\d+")) { // Validar que sea un número entero (positivo o negativo)
-                    pilotos[1][i] = posiciones; // Guardar posiciones adelantadas
-                    break;
+                if (posiciones.matches("-?\\d+")) { // Validar que sea un número entero
+                    int posicionesInt = Integer.parseInt(posiciones);
+                    // Verificar que las posiciones no salgan del rango permitido
+                    if (posicionesInt >= -posicionesMaxPerder && posicionesInt <= posicionesMaxAdelantar) {
+                        pilotos[1][i] = posiciones; // Guardar posiciones válidas
+                        break;
+                    } else {
+                        System.out.print("Error. El número de posiciones debe estar entre " + (-posicionesMaxPerder) + " y " + posicionesMaxAdelantar + ": ");
+                    }
                 } else {
                     System.out.print("Error. Ingrese un número válido para las posiciones: ");
                 }
